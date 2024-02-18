d749302e1ac94fd742e94edbdf9f35502b8c3e06
Richard Ly <rly758@gmail.com>
2024-02-18 11:24:59 -0800

    Tested calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,49 @@
-function calculator() {};
+
+function isNumber(num) {
+    return typeof num === "number" ? true : false;
+}
+
+function add(a,b) {
+    if (isNumber(a) && isNumber(b)) {
+        return a+b;
+    }
+    else{
+        return "Arguments contain a non-number";
+    }
+}
+
+function subtract(a,b) {
+    if (isNumber(a) && isNumber(b)) {
+        return a-b;
+    }
+    else{
+        return "Arguments contain a non-number";
+    }
+}
+
+function divide(a,b) {
+    if (isNumber(a) && isNumber(b)) {
+        return a/b;
+    }
+    else{
+        return "Arguments contain a non-number";
+    }
+}
+
+function multiply(a,b) {
+    if (isNumber(a) && isNumber(b)) {
+        return a*b;
+    }
+    else{
+        return "Arguments contain a non-number";
+    }
+}
+
+const calculator = {
+    add,
+    subtract,
+    divide,
+    multiply,
+};
 
 module.exports = calculator;
