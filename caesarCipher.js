07265b875662b47d218037bffb13b6096251829b
Richard Ly <rly758@gmail.com>
2024-02-18 11:57:35 -0800

    Tested caesarCipher

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,23 @@
-function caesarCipher() {};
+function caesarCipher(str,shift) {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/[a-z]/)) {
+            const index = lowerAlphabet.indexOf(str[i]);
+            result += lowerAlphabet[(index + shift) % 26];
+        } 
+        else if (str[i].match(/[A-Z]/)) {
+            const index = upperAlphabet.indexOf(str[i]);
+            result += upperAlphabet[(index + shift) % 26];
+        }
+        else {
+            result += str[i];
+        }
+    }
+
+    return result;
+};
 
 module.exports = caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,3 +1,34 @@
 const caesarCipher = require("./caesarCipher");
 
-test("test", () => {});
+describe("Caesar Cipher", () => {
+    test("'a', 1 shift", () => {
+      expect(caesarCipher("a", 1)).toBe("b");
+    });
+    test("'a', 3 shift", () => {
+      expect(caesarCipher("a", 3)).toBe("d");
+    });
+    test("'A', 1 shift", () => {
+      expect(caesarCipher("A", 1)).toBe("B");
+    });
+    test("'A', 3 shift", () => {
+      expect(caesarCipher("A", 3)).toBe("D");
+    });
+    test("'Test', shift 1", () => {
+      expect(caesarCipher("Test", 1)).toBe("Uftu");
+    });
+    test("'TeSt', shift 3", () => {
+      expect(caesarCipher("TeSt", 3)).toBe("WhVw");
+    });
+    test("'ZOO', shift 1", () => {
+      expect(caesarCipher("ZOO", 1)).toBe("APP");
+    });
+    test("'Z O O', shift 1", () => {
+        expect(caesarCipher("Z O O", 1)).toBe("A P P");
+    });
+    test("'!Z@o#O$', shift 1", () => {
+        expect(caesarCipher("!Z@o#O$", 1)).toBe("!A@p#P$");
+    }); 
+    test("'1 2 3', shift 1 ", () => {
+        expect(caesarCipher("1 2 3", 1)).toBe("1 2 3");
+    });
+  });
