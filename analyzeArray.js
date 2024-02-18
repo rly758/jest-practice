4b178a858c053fcf2e2c24fb0fc1230eee32440a
Richard Ly <rly758@gmail.com>
2024-02-18 12:24:48 -0800

    Tested analyzeArray

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,3 +1,21 @@
-function analyzeArray() {};
+function analyzeArray(arr) {
+    if (arr.length === 0) {
+        return "empty array"
+    }
+
+    const length = arr.length;
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const average = arr.reduce((a,b) => a + b, 0) / length;
+
+    const stats = {
+        average,
+        min,
+        max,
+        length,
+    };
+
+    return stats;
+};
 
 module.exports = analyzeArray;
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,3 +1,13 @@
 const analyzeArray = require("./analyzeArray");
 
-test("test", () => {});
+describe("analyzeArray", () => {
+    test("[1,8,3,4,2,6] should be {average: 4, min: 1, max: 8, length: 6}", () => {
+        expect(analyzeArray([1,8,3,4,2,6])).toMatchObject({average: 4, min: 1, max: 8, length: 6});
+    });
+    test("[0] should be {average: 0, min: 0, max: 0, length: 1}", () => {
+        expect(analyzeArray([0])).toMatchObject({average: 0, min: 0, max: 0, length: 1});
+    });
+    test("[] should be 'empty array'", () => {
+        expect(analyzeArray([])).toBe("empty array");
+    });
+});
