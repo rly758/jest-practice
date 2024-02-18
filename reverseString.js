6c44b78bfe7393f3d50a1bdde6d4223316c5b3c7
Richard Ly <rly758@gmail.com>
2024-02-18 11:08:06 -0800

    Tested reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+    if (typeof str !== "string") {
+        return "not a string";
+    }
+
+    return str.split("").reverse().join("");
+}
 
 module.exports = reverseString;
--- reverseString.test.js
+++ reverseString.test.js
@@ -1,3 +1,19 @@
 const reverseString = require("./reverseString");
 
-test("test", () => {});
+describe("reverseString", () => {
+    test("'test' becomes 'tset'", () => {
+        expect(reverseString('test')).toBe('tset');
+      });
+      
+      test("'try some spaces' becomes 'secaps emos yrt'", () => {
+        expect(reverseString("try some spaces")).toBe("secaps emos yrt");
+      });
+      
+      test("'12345' becomes '54321'", () => {
+        expect(reverseString("12345")).toBe("54321");
+      });
+      
+      test("12345 becomes 'not a string'", () => {
+        expect(reverseString(12345)).toBe("not a string");
+      });
+});
